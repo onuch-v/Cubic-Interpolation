05c2af338cd1b2107322f13a5e313101c147c68c
Vadim Onuchin <42718970+ValexCorp@users.noreply.github.com>
2019-02-17 17:14:47 +0300

    Updated algorithm Interpolation
    Added new class CubicInterpolation for using method interpolate on one X get one Y like in MathCAD.

---
+++ MathLibrary/Example.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathLibrary;
+
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start.\n");
+
+            ExampleWithArrays(out double[] x11, out double[] y11);
+
+            ExampleWithoutArrays(out double[] x12, out double[] y12);
+
+            Console.WriteLine();
+            Console.WriteLine("Good Interpolation. Use It.");
+            Console.WriteLine("|-------|-------|-------|-------|-------|");
+            Console.WriteLine("|   i   |   x1  |   x2  |   y1  |   y2  |");
+            Console.WriteLine("|-------|-------|-------|-------|-------|");
+            
+            for (long i = 0; i < x11.LongLength; i++)
+            {
+                Console.Write("|");
+                Console.Write("{0,-7:F2}|", i);
+                Console.Write(x11[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", x11[i]);
+                Console.Write(x12[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", x12[i]);
+                Console.Write(y11[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", y11[i]);
+                Console.Write(y12[i] < 0 ? "{0,-7:F2}|" : " {0,-6:F2}|", y12[i]);
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("|-------|-------|-------|-------|-------|");
+            Console.WriteLine("Enjoy!");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x1">Interpolated Data OX (new abscissa)</param>
+        /// <param name="y1">Interpolated Data OY (new ordinate)</param>
+        static void ExampleWithArrays(out double[] x1, out double[] y1)
+        {
+            Console.WriteLine("Example With Arrays : Start.\n");
+            x1 = null;
+            y1 = null;
+            int N = 40;
+            double Fs = 20.0;
+
+            double[] x = new double[N];
+            double[] y = new double[N];
+
+            x1 = new double[2 * N - 1];                             /* new data X */
+
+            for (int i = 0; i < N; i++)
+            {
+                x[i] = i / Fs;
+                y[i] = Math.Sin(2.0 * Math.PI * x[i]);
+            }
+
+            for (int i = 0; i < x1.Length; i++)
+                x1[i] = i / (2.0 * Fs);
+
+            y1 = Interpolation.CubicInterpolation(x, y, x1);        /* new data Y */
+
+            Console.WriteLine("Example With Arrays : Enjoy.\n");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x1">Interpolated Data OX (new abscissa)</param>
+        /// <param name="y1">Interpolated Data OY (new ordinate)</param>
+        static void ExampleWithoutArrays(out double[] x1, out double[] y1)
+        {
+            Console.WriteLine("Example Without Arrays : Start.\n");
+
+            x1 = null;
+            y1 = null;
+
+            int N = 40;
+            double Fs = 20.0;
+
+            double[] x = new double[N];
+            double[] y = new double[N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                x[i] = i / Fs;
+                y[i] = Math.Sin(2.0 * Math.PI * x[i]);
+            }
+
+            CubicInterpolation cubic = new CubicInterpolation();
+            cubic.CalcCoefficients(x, y);
+
+            x1 = new double[2 * N - 1];
+            y1 = new double[2 * N - 1];
+            for (int i = 0; i < 2 * N - 1; i++)
+            {
+                x1[i] = i / (2.0 * Fs);
+                y1[i] = cubic.Interpolate(x1[i]);
+            }
+
+            Console.WriteLine("WARNING!");
+            Console.WriteLine("\tYou will do this at your own risk.");
+            double newX = -2.0;                                                                     /* Never do it */
+            double newY = cubic.Interpolate(newX);                                                  /* Never do it */
+            Console.WriteLine("\tNew (X < sourceX[0], Y) = ({0,3:F2} ; {1,3:F2})", newX, newY);
+
+            newX = 4 * N / Fs;                                                                      /* Never do it */
+            newY = cubic.Interpolate(newX);                                                         /* Never do it */
+            Console.WriteLine("\tNew (X > sourceX[end], Y) = ({0,3:F2} ; {1,3:F2})", newX, newY);
+
+            Console.WriteLine("Example Without Arrays : Enjoy.\n");
+        }
+    }
+}
--- MathLibrary/Interpolation.cs
+++ MathLibrary/Interpolation.cs
@@ -4,6 +4,183 @@ using System.Linq;
 namespace MathLibrary
 {
     /// <summary>
+    /// Cubic Interpolation
+    /// You can see the full algotithm at "de Boor, C., A Practical Guide to Splines, Springer-Verlag, 1978".
+    /// Author: Vadim Onuchin, onuch-v@ya.ru
+    /// © Valex Corp. https://github.com/ValexCorp 
+    /// Moscow, 2019
+    /// </summary>
+    public class CubicInterpolation
+    {
+        private double[] sourceX = null;
+        private double[] sourceY = null;
+        private long N = 0;
+        private double[][] coefs = null;
+
+        /// <summary>
+        /// </summary>
+        public CubicInterpolation()
+        {
+        }
+
+        /// <summary>
+        /// There is Calculate Coefficients of spline
+        /// </summary>
+        /// <param name="sourceX">The source data OX (abscissa)</param>
+        /// <param name="sourceY">The source data OY (ordinate)</param>
+        /// <returns>
+        /// -1: sourceX.LongLength != sourceY.LongLength
+        /// -2: does not enough source data
+        /// </returns>
+        public int CalcCoefficients(double[] sourceX, double[] sourceY)
+        {
+            
+            N = sourceX.LongLength;
+            if (sourceX.LongLength != sourceY.LongLength)
+                return -1;
+
+            if (sourceX.LongLength <= 3)
+                return -2;
+
+            this.sourceX = sourceX;
+            this.sourceY = sourceY;
+
+            /*
+             * Spline[i] = f[i] + b[i]*(x - x[i]) + c[i]*(x - x[i])^2 + d[i]*(x - x[i])^3
+             * First: We prepare data for algorithm by calculate dx[i]. If dx[i] equal to zero then function return null.
+             * Second: We need calculate coefficients b[i]. 
+             * b[i] = 3 * ( (f[i] - f[i - 1])*dx[i]/dx[i - 1] + (f[i + 1] - f[i])*dx[i - 1]/dx[i] ),  i = 1, ... , N - 2
+             * How calculate b[0] and b[N - 1] you can see below. And b can be find by means of tridiagonal matrix A[N, N].
+             * 
+             * A[N, N] - Tridiagonal Matrix:
+             *      beta(0)     gama(0)     0            0           0   ...
+             *      alfa(1)     beta(1)     gama(1)      0           0   ...
+             *      0           alfa(2)     beta(2)     gama(2)      0
+             *      ...
+             * A*x=b
+             * We calculate inverse of tridiagonal matrix by Gauss method and transforming equation A*x=b to the form I*x=b, where I - Identity matrix.
+             * Fird: Now we can found coefficients c[i], d[i] where i = 0, ... , N - 2
+             */
+
+            long Nx = N - 1;
+            double[] dx = new double[Nx];
+
+            double[] b = new double[N];
+            double[] alfa = new double[N];
+            double[] beta = new double[N];
+            double[] gama = new double[N];
+
+            coefs = new double[4][];
+            for (long i = 0; i < 4; i++)
+                coefs[i] = new double[Nx];
+
+            for (long i = 0; i + 1 <= Nx; i++)
+            {
+                dx[i] = sourceX[i + 1] - sourceX[i];
+                if (dx[i] == 0.0)
+                    return -1;
+            }
+
+            for (long i = 1; i + 1 <= Nx; i++)
+            {
+                b[i] = 3.0 * (dx[i] * ((sourceY[i] - sourceY[i - 1]) / dx[i - 1]) + dx[i - 1] * ((sourceY[i + 1] - sourceY[i]) / dx[i]));
+            }
+
+            b[0] = ((dx[0] + 2.0 * (sourceX[2] - sourceX[0])) * dx[1] * ((sourceY[1] - sourceY[0]) / dx[0]) +
+                        Math.Pow(dx[0], 2.0) * ((sourceY[2] - sourceY[1]) / dx[1])) / (sourceX[2] - sourceX[0]);
+
+            b[N - 1] = (Math.Pow(dx[Nx - 1], 2.0) * ((sourceY[N - 2] - sourceY[N - 3]) / dx[Nx - 2]) + (2.0 * (sourceX[N - 1] - sourceX[N - 3])
+                + dx[Nx - 1]) * dx[Nx - 2] * ((sourceY[N - 1] - sourceY[N - 2]) / dx[Nx - 1])) / (sourceX[N - 1] - sourceX[N - 3]);
+
+            beta[0] = dx[1];
+            gama[0] = sourceX[2] - sourceX[0];
+            beta[N - 1] = dx[Nx - 1];
+            alfa[N - 1] = (sourceX[N - 1] - sourceX[N - 3]);
+            for (long i = 1; i < N - 1; i++)
+            {
+                beta[i] = 2.0 * (dx[i] + dx[i - 1]);
+                gama[i] = dx[i];
+                alfa[i] = dx[i - 1];
+            }
+            double c = 0.0;
+            for (long i = 0; i < N - 1; i++)
+            {
+                c = beta[i];
+                b[i] /= c;
+                beta[i] /= c;
+                gama[i] /= c;
+
+                c = alfa[i + 1];
+                b[i + 1] -= c * b[i];
+                alfa[i + 1] -= c * beta[i];
+                beta[i + 1] -= c * gama[i];
+            }
+
+            b[N - 1] /= beta[N - 1];
+            beta[N - 1] = 1.0;
+            for (long i = N - 2; i >= 0; i--)
+            {
+                c = gama[i];
+                b[i] -= c * b[i + 1];
+                gama[i] -= c * beta[i];
+            }
+
+            for (long i = 0; i < Nx; i++)
+            {
+                double dzzdx = (sourceY[i + 1] - sourceY[i]) / Math.Pow(dx[i], 2.0) - b[i] / dx[i];
+                double dzdxdx = b[i + 1] / dx[i] - (sourceY[i + 1] - sourceY[i]) / Math.Pow(dx[i], 2.0);
+                coefs[0][i] = (dzdxdx - dzzdx) / dx[i];
+                coefs[1][i] = (2.0 * dzzdx - dzdxdx);
+                coefs[2][i] = b[i];
+                coefs[3][i] = sourceY[i];
+            }
+            return 0;
+        }
+
+        /// <summary> 
+        /// I do not recomend use this function when <code>newX > sourceX[N - 1]</code> and <code>newX &lt; sourceX[0]</code>
+        /// </summary>
+        /// <param name="newX"></param>
+        /// <returns></returns>
+        public double Interpolate(double newX)
+        {
+            double newY = 0.0;
+
+            for (long i = 0; i < N; i++)
+            {
+                if (newX == sourceX[i])
+                    return sourceY[i];
+            }
+
+            double h = 0.0;
+
+            if (newX < sourceX[0])
+            {
+                h = newX - sourceX[0];
+                newY = coefs[3][0] + h * (coefs[2][0] + h * (coefs[1][0] + h * coefs[0][0] / 3.0) / 2.0);
+                return newY;
+            }
+            if (newX > sourceX[N - 1])
+            {
+                h = newX - sourceX[N - 1];
+                newY = coefs[3][N - 2] + h * (coefs[2][N - 2] + h * (coefs[1][N - 2] + h * coefs[0][N - 2] / 3.0) / 2.0);
+                return newY;
+            }
+
+            for (long i = 0; i < N - 1; i++)
+            {
+                if (newX < sourceX[i + 1])
+                {
+                    h = newX - sourceX[i];
+                    newY = coefs[3][i] + h * (coefs[2][i] + h * (coefs[1][i] + h * coefs[0][i] / 3.0) / 2.0);
+                    break;
+                }
+            }
+            return newY;
+        }
+
+    }
+    /// <summary>
     /// Interpolation
     /// </summary>
     public class Interpolation
